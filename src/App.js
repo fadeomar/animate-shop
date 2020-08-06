80329d62faa84c601d15b59c64929921e872e0b9
fadeomar <fadeomar2015@gmail.com>
2020-08-06 23:19:11 +0300

    install initial config with eslint and pritter

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hi from app</h1>
+    </div>
+  );
+}
+
+export default App;
